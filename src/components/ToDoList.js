0b414fc0f702fc4c784e2ade326f7db23c6ee405
Mario Acosta <macosta2015@my.fit.edu>
2023-05-12 19:48:15 -0400

    Todo list is working

--- src/components/ToDoList.js
+++ src/components/ToDoList.js
@@ -3,11 +3,13 @@ import Todo from './ToDo'
 
 
 const ToDoList = ({todos}) => {
-    console.log(todos)
+    // console.log(todos)
     return (
         <div className="todo-container">
             <ul className="todo-list">
-                <Todo/>
+                {todos.map((todo)=>(
+                    <Todo text={todo.text} id={todo.id}/>
+                ))}
             </ul>
         </div>
     );
